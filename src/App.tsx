04f506f2b3aeeba068913522e9a121f40b425227
valec3 <v1c70rotakmer@gmail.com>
2025-06-15 22:43:22 -0500

    Add: MUI Icons Material dependency and update AppBar structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,104 @@
 import { Routes, Route, Link } from "react-router-dom";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Box
-} from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import Home from "./pages/Home/Home";
 import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <>
-<AppBar
-  position="static"
-  sx={{
-    backgroundColor: "#ffffff",
-    boxShadow: "none",
-    padding: "10px 20px",
-    display: "flex"
-  }}
->
-  <Toolbar sx={{ justifyContent: "space-between" }}>
-    
-    {/* Izquierda: Logo AESI */}
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <img
-        src={`/assets/Home/Aesi logo.png`}
-        alt="AESI"
-        style={{
-          height: "40px",
-          width: "auto",
-          maxWidth: "120px",
-          objectFit: "contain"
-        }}
-      />
-    </Box>
-
-    {/* Centro: iconos1 + texto + iconos2 */}
-    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-      <img
-        src={`/assets/Home/iconos1.png`}
-        alt="Iconos izquierda"
-        style={{
-          height: "50px",
-          width: "auto",
-          maxWidth: "100px",
-          objectFit: "contain",
-          marginRight: "18px"
-        }}
-      />
-      <Typography
-        variant="h4"
+      <AppBar
+        position="static"
         sx={{
-          fontWeight: "bold",
-          color: "#000",
-          textAlign: "center"
+          backgroundColor: "#ffffff",
+          boxShadow: "none",
+          padding: "10px 20px",
+          display: "flex",
         }}
       >
-        Bolsa de empleo inclusivo
-      </Typography>
-      <img
-        src={`/assets/Home/iconos2.png`}
-        alt="Iconos derecha"
-        style={{
-          height: "50px",
-          width: "auto",
-          maxWidth: "100px",
-          objectFit: "contain",
-          marginLeft: "18px"
-        }}
-      />
-    </Box>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          {/* Izquierda: Logo AESI */}
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={`/assets/Home/Aesi logo.png`}
+              alt="AESI"
+              style={{
+                height: "40px",
+                width: "auto",
+                maxWidth: "120px",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
 
-    {/* Derecha: Botones */}
-    <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-      <Button color="inherit" component={Link} to="/login"
-      sx={{
-          backgroundColor: "#fff",
-          color: "#000000", 
-          "&:hover": {
-            backgroundColor: "#fff"
-          }
-        }}>
-        Login
-      </Button>
-      <Button
-        component={Link}
-        to="/registro"
-        sx={{
-          backgroundColor: "#c62828",
-          color: "#fff",
-          "&:hover": {
-            backgroundColor: "#b71c1c"
-          }
-        }}
-        variant="contained"
-      >
-        Regístrate
-      </Button>
-    </Box>
-  </Toolbar>
-</AppBar>
+          {/* Centro: iconos1 + texto + iconos2 */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <img
+              src={`/assets/Home/iconos1.png`}
+              alt="Iconos izquierda"
+              style={{
+                height: "50px",
+                width: "auto",
+                maxWidth: "100px",
+                objectFit: "contain",
+                marginRight: "18px",
+              }}
+            />
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: "bold",
+                color: "#000",
+                textAlign: "center",
+              }}
+            >
+              Bolsa de empleo inclusivo
+            </Typography>
+            <img
+              src={`/assets/Home/iconos2.png`}
+              alt="Iconos derecha"
+              style={{
+                height: "50px",
+                width: "auto",
+                maxWidth: "100px",
+                objectFit: "contain",
+                marginLeft: "18px",
+              }}
+            />
+          </Box>
+
+          {/* Derecha: Botones */}
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <Button
+              color="inherit"
+              component={Link}
+              to="/login"
+              sx={{
+                backgroundColor: "#fff",
+                color: "#000000",
+                "&:hover": {
+                  backgroundColor: "#fff",
+                },
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              component={Link}
+              to="/registro"
+              sx={{
+                backgroundColor: "#c62828",
+                color: "#fff",
+                "&:hover": {
+                  backgroundColor: "#b71c1c",
+                },
+              }}
+              variant="contained"
+            >
+              Regístrate
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
 
       <Box sx={{ mt: 2 }}>
         <Routes>
@@ -111,6 +108,7 @@ function App() {
         </Routes>
       </Box>
       <Footer />
+    </>
   );
 }
 
